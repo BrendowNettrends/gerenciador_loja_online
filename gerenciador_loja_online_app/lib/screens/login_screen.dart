71bc45eba96c5b4696295c5dea7c077a1147ef4a
BrendowLincoln <linconlbrendow@gmail.com>
2020-10-03 16:15:32 -0300

    Listener de usário

--- gerenciador_loja_online_app/lib/screens/login_screen.dart
+++ gerenciador_loja_online_app/lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gerenciador_loja_online/blocs/login_bloc.dart';
 import 'package:gerenciador_loja_online/widgets/input_field.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -7,43 +8,62 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  final _loginBloc = LoginBloc();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey,
-      body: SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Icon(
-                Icons.store_mall_directory,
-                color: Colors.greenAccent,
-                size: 160,
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Container(),
+          SingleChildScrollView(
+            child: Container(
+              margin: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Icon(
+                    Icons.store_mall_directory,
+                    color: Colors.greenAccent,
+                    size: 160,
+                  ),
+                  InputField(
+                    icon: Icons.person_outline,
+                    hint: "Usuário",
+                    obscure: false,
+                    stream: _loginBloc.outEmail,
+                    onChanged: _loginBloc.changeEmail,
+                  ),
+                  InputField(
+                    icon: Icons.lock_outline,
+                    hint: "Senha",
+                    obscure: true,
+                    stream: _loginBloc.outPassword,
+                    onChanged: _loginBloc.changePassword,
+                  ),
+                  SizedBox(height: 32,),
+                  StreamBuilder<bool>(
+                    stream: _loginBloc.outSubmitValid,
+                    builder: (context, snapshot) {
+                      return SizedBox(
+                        child: RaisedButton(
+                          color: Colors.greenAccent,
+                          child: Text("Entrar"),
+                          onPressed: snapshot.hasData ? _loginBloc.submit : null,
+                          textColor: Colors.white,
+                          disabledColor: Colors.greenAccent.withAlpha(140),
+                        ),
+                      );
+                    }
+                  )
+                ],
               ),
-              InputField(
-                icon: Icons.person_outline,
-                hint: "Usuário",
-                obscure: false,
-              ),
-              InputField(
-                icon: Icons.lock_outline,
-                hint: "Senha",
-                obscure: true,
-              ),
-              SizedBox(height: 32,),
-              SizedBox(
-                child: RaisedButton(
-                  color: Colors.greenAccent,
-                  child: Text("Entrar"),
-                  onPressed: () {},
-                  textColor: Colors.white,
-                ),
-              )
-            ],
+            )
           ),
-        )
+        ],
       )
     );
   }
