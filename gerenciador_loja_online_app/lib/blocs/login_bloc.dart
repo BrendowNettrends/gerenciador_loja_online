ef87b31e4bad130d062d341f5436841a68028304
BrendowLincoln <linconlbrendow@gmail.com>
2020-10-03 21:28:36 -0300

    Finalizando Login

--- gerenciador_loja_online_app/lib/blocs/login_bloc.dart
+++ gerenciador_loja_online_app/lib/blocs/login_bloc.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:gerenciador_loja_online/validators/login_validators.dart';
 import 'package:rxdart/rxdart.dart';
@@ -15,11 +18,27 @@ class LoginBloc extends BlocBase with LoginValidators {
   Stream<String> get outPassword => _passwordController.stream.transform(validatePassword);
   Stream<LoginState> get outState => _stateController.stream;
 
+  Stream<bool> get outSubmitValid => Observable.combineLatest2(
+      outEmail, outPassword, (a, b) => true
+  );
+
+  Function(String) get changeEmail => _emailController.sink.add;
+  Function(String) get changePassword => _passwordController.sink.add;
+
+  StreamSubscription _streamSubscription;
+
+
   LoginBloc() {
-    FirebaseAuth.instance.onAuthStateChanged.listen((user) {
+
+    FirebaseAuth.instance.signOut();
+    _streamSubscription = FirebaseAuth.instance.onAuthStateChanged.listen((user) async {
       if(user != null){
-        print("logou!");
-        FirebaseAuth.instance.signOut();
+        if(await verifyPrivileges(user)) {
+          _stateController.add(LoginState.SUCCESS);
+        } else {
+          FirebaseAuth.instance.signOut();
+          _stateController.add(LoginState.FAIL);
+        }
       } else {
         _stateController.add(LoginState.IDLE);
       }
@@ -44,18 +63,28 @@ class LoginBloc extends BlocBase with LoginValidators {
 
   }
 
-  Stream<bool> get outSubmitValid => Observable.combineLatest2(
-      outEmail, outPassword, (a, b) => true
-  );
-  
-  Function(String) get changeEmail => _emailController.sink.add;
-  Function(String) get changePassword => _passwordController.sink.add;
+  Future<bool> verifyPrivileges(FirebaseUser user) async {
+
+    return await Firestore.instance.collection("admin").document(user.uid).get().then((doc){
+      if(doc.data != null) {
+        return true;
+      } else {
+        return false;
+      }
+    }).catchError((e){
+      return false;
+    });
+
+  }
+
 
   @override
   void dispose() {
     _emailController.close();
     _passwordController.close();
     _stateController.close();
+
+    _streamSubscription.cancel();
   }
 
 
--- gerenciador_loja_online_app/lib/screens/login_screen.dart
+++ gerenciador_loja_online_app/lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gerenciador_loja_online/blocs/login_bloc.dart';
+import 'package:gerenciador_loja_online/screens/home_screen.dart';
 import 'package:gerenciador_loja_online/widgets/input_field.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -11,60 +12,100 @@ class _LoginScreenState extends State<LoginScreen> {
 
   final _loginBloc = LoginBloc();
 
+
+  @override
+  void initState() {
+    super.initState();
+
+    _loginBloc.outState.listen((state) {
+
+      switch(state){
+        case LoginState.SUCCESS:
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => HomeScreen())
+          );
+          break;
+        case LoginState.FAIL:
+          showDialog(context: context, builder: (context) => AlertDialog(
+            title: Text("Erro"),
+            content: Text("Você não possui os privilegios necessários"),
+          ));
+          break;
+        case LoginState.IDLE:
+        case LoginState.LOADING:
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey,
-      body: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Container(),
-          SingleChildScrollView(
-            child: Container(
-              margin: EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: StreamBuilder<LoginState>(
+        stream: _loginBloc.outState,
+        builder: (context, snapshot) {
+          print(snapshot.data);
+          switch(snapshot.data) {
+            case LoginState.LOADING:
+              return Center(child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(Colors.greenAccent),
+              ),);
+            case LoginState.FAIL:
+            case LoginState.SUCCESS:
+            case LoginState.IDLE:
+              return Stack(
+                alignment: Alignment.center,
                 children: <Widget>[
-                  Icon(
-                    Icons.store_mall_directory,
-                    color: Colors.greenAccent,
-                    size: 160,
-                  ),
-                  InputField(
-                    icon: Icons.person_outline,
-                    hint: "Usuário",
-                    obscure: false,
-                    stream: _loginBloc.outEmail,
-                    onChanged: _loginBloc.changeEmail,
-                  ),
-                  InputField(
-                    icon: Icons.lock_outline,
-                    hint: "Senha",
-                    obscure: true,
-                    stream: _loginBloc.outPassword,
-                    onChanged: _loginBloc.changePassword,
-                  ),
-                  SizedBox(height: 32,),
-                  StreamBuilder<bool>(
-                    stream: _loginBloc.outSubmitValid,
-                    builder: (context, snapshot) {
-                      return SizedBox(
-                        child: RaisedButton(
-                          color: Colors.greenAccent,
-                          child: Text("Entrar"),
-                          onPressed: snapshot.hasData ? _loginBloc.submit : null,
-                          textColor: Colors.white,
-                          disabledColor: Colors.greenAccent.withAlpha(140),
+                  Container(),
+                  SingleChildScrollView(
+                      child: Container(
+                        margin: EdgeInsets.all(16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Icon(
+                              Icons.store_mall_directory,
+                              color: Colors.greenAccent,
+                              size: 160,
+                            ),
+                            InputField(
+                              icon: Icons.person_outline,
+                              hint: "Usuário",
+                              obscure: false,
+                              stream: _loginBloc.outEmail,
+                              onChanged: _loginBloc.changeEmail,
+                            ),
+                            InputField(
+                              icon: Icons.lock_outline,
+                              hint: "Senha",
+                              obscure: true,
+                              stream: _loginBloc.outPassword,
+                              onChanged: _loginBloc.changePassword,
+                            ),
+                            SizedBox(height: 32,),
+                            StreamBuilder<bool>(
+                                stream: _loginBloc.outSubmitValid,
+                                builder: (context, snapshot) {
+                                  return SizedBox(
+                                    child: RaisedButton(
+                                      color: Colors.greenAccent,
+                                      child: Text("Entrar"),
+                                      onPressed: snapshot.hasData ? _loginBloc.submit : null,
+                                      textColor: Colors.white,
+                                      disabledColor: Colors.greenAccent.withAlpha(140),
+                                    ),
+                                  );
+                                }
+                            )
+                          ],
                         ),
-                      );
-                    }
-                  )
+                      ),
+                  ),
                 ],
-              ),
-            )
-          ),
-        ],
-      )
+              );
+          }
+        }
+      ),
     );
   }
 }
