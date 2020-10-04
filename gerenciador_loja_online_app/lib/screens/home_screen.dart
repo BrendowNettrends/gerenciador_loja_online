b2a40e88f5101fd8c0cda10b4772805e10e5b213
BrendowLincoln <linconlbrendow@gmail.com>
2020-10-03 23:44:57 -0300

    Sistema de páginas

--- gerenciador_loja_online_app/lib/screens/home_screen.dart
+++ gerenciador_loja_online_app/lib/screens/home_screen.dart
@@ -5,8 +5,76 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  PageController _pageController;
+  int _page = 0;
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    _pageController = PageController();
+  }
+
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.greenAccent,
+          primaryColor: Colors.white,
+          textTheme: Theme.of(context).textTheme.copyWith(
+            caption: TextStyle(color: Colors.white54)
+          )
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _page,
+          onTap: (p) {
+            _pageController.animateToPage(
+                p,
+                duration: Duration(milliseconds: 500),
+                curve: Curves.ease
+            );
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text("Clientes")
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart),
+                title: Text("Pedidos")
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.list),
+                title: Text("Produtos")
+            )
+          ]
+        ),
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (p) {
+          setState(() {
+            _page = p;
+          });
+        },
+        children: <Widget>[
+          Container(color: Colors.blue),
+          Container(color: Colors.amber),
+          Container(color: Colors.deepPurpleAccent)
+        ],
+      )
+    );
   }
 }
