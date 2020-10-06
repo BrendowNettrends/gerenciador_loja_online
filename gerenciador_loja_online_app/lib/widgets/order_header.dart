88ff90a89c259d27199cbd44c44185302ac7ebcb
BrendowNettrends <brendow.lincoln@nettrends.com.br>
2020-10-06 13:28:53 -0300

    Mudando Status do pedido

--- gerenciador_loja_online_app/lib/widgets/order_header.dart
+++ gerenciador_loja_online_app/lib/widgets/order_header.dart
@@ -1,25 +1,38 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:gerenciador_loja_online/blocs/user_blocs.dart';
 
 class OrderHeader extends StatelessWidget {
+
+  final DocumentSnapshot order;
+
+  OrderHeader(this.order);
+
   @override
   Widget build(BuildContext context) {
+
+    final _userBloc = BlocProvider.of<UserBloc>(context);
+
+    final _user = _userBloc.getUser(order.data["clientID"]);
+
     return Row(
       children: <Widget>[
         Expanded(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text("Brendow"),
-              Text("Rua X, N12")
+              Text("${_user["name"]}"),
+              Text("${_user["email"]}")
             ],
           ),
         ),
         Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[
-            Text("Preço Produtos", style: TextStyle(fontWeight: FontWeight.w500),),
-            Text("Preço total")
+            Text("Produtos: R\$${order.data["productsPrice"].toStringAsFixed(2)}", style: TextStyle(fontWeight: FontWeight.w500),),
+            Text("Total: R\$${order.data["totalPrice"].toStringAsFixed(2)}", style: TextStyle(fontWeight: FontWeight.w500),)
           ],
         )
       ],
