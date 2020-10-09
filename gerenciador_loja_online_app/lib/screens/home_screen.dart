cdf83cd507bf68084407602e4ea8843057db9b1a
BrendowNettrends <brendow.lincoln@nettrends.com.br>
2020-10-09 12:38:40 -0300

    Carregando os Dados de Categoria

--- gerenciador_loja_online_app/lib/screens/home_screen.dart
+++ gerenciador_loja_online_app/lib/screens/home_screen.dart
@@ -6,6 +6,7 @@ import 'package:gerenciador_loja_online/blocs/user_blocs.dart';
 import 'package:gerenciador_loja_online/tabs/orders_tab.dart';
 import 'package:gerenciador_loja_online/tabs/products_tab.dart';
 import 'package:gerenciador_loja_online/tabs/users_tab.dart';
+import 'package:gerenciador_loja_online/widgets/edit_category_dialog.dart';
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -100,35 +101,45 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget _buildFloating() {
     switch(_page){
-      case 0:
-        return null;
-      case 1:
-        return SpeedDial(
-          child: Icon(Icons.sort),
-          backgroundColor: Colors.greenAccent,
-          overlayOpacity: 0.4,
-          overlayColor: Colors.black,
+    case 0:
+      return null;
+    case 1:
+      return SpeedDial(
+        child: Icon(Icons.sort),
+        backgroundColor: Colors.greenAccent,
+        overlayOpacity: 0.4,
+        overlayColor: Colors.black,
 
-          children: [
-            SpeedDialChild(
-              child: Icon(Icons.arrow_downward, color: Colors.greenAccent,),
-              backgroundColor: Colors.white,
-              label: "Concluidos Abaixo",
-              labelStyle: TextStyle(fontSize: 14),
-              onTap: () {
-                _ordersBloc.setOrderCriteria(SortCriteria.READY_LAST);
-              },
-            ),
-            SpeedDialChild(
-              child: Icon(Icons.arrow_upward, color: Colors.greenAccent,),
-              backgroundColor: Colors.white,
-              label: "Concluidos Acima",
-              labelStyle: TextStyle(fontSize: 14),
-              onTap: () {
-                _ordersBloc.setOrderCriteria(SortCriteria.READY_FIRST);
-              },
-            )
-          ],
+        children: [
+          SpeedDialChild(
+            child: Icon(Icons.arrow_downward, color: Colors.greenAccent,),
+            backgroundColor: Colors.white,
+            label: "Concluidos Abaixo",
+            labelStyle: TextStyle(fontSize: 14),
+            onTap: () {
+              _ordersBloc.setOrderCriteria(SortCriteria.READY_LAST);
+            },
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.arrow_upward, color: Colors.greenAccent,),
+            backgroundColor: Colors.white,
+            label: "Concluidos Acima",
+            labelStyle: TextStyle(fontSize: 14),
+            onTap: () {
+              _ordersBloc.setOrderCriteria(SortCriteria.READY_FIRST);
+            },
+          )
+        ],
+      );
+      case 2:
+        return FloatingActionButton(
+            child: Icon(Icons.add),
+            backgroundColor: Colors.greenAccent,
+            onPressed: () {
+              showDialog(context: context,
+                builder: (context) => EditCategoryDialog()
+              );
+            }
         );
     }
   }
